759859827b38222350a3e4a4670eeb0e603a2b04
Hecdin <Hecdinnicola@outlook.com>
2024-10-04 08:38:07 -0400

    branch dev

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config'
+import Icons from 'unplugin-icons/vite'
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
+})
